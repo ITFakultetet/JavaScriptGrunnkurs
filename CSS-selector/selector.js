76b98d60b2125b55b90d82bf623b92de5673c816
terjebh <terje@itfakultetet.no>
2021-10-16 19:39:04 +0200

    web

---
+++ CSS-selector/selector.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+let tittel = document.querySelector("h1"); 
+tittel.innerHTML="Velkommen til denne siden";
+tittel.style.backgroundColor="navy";
+tittel.style.color="white";
+tittel.style.padding="5px";
+tittel.style.textAlign=""
+
+
+
+}
