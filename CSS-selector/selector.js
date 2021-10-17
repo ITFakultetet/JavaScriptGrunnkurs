c5095d49d56c8c8d8e156258209a70e2e906c99c
terjebh <terje@itfakultetet.no>
2021-10-17 19:52:55 +0200

    selectors

--- CSS-selector/selector.js
+++ CSS-selector/selector.js
@@ -1,12 +1,26 @@
 window.onload = function () {
 
-let tittel = document.querySelector("h1"); 
-tittel.innerHTML="Velkommen til denne siden";
-tittel.style.backgroundColor="navy";
-tittel.style.color="white";
-tittel.style.padding="10px";
-tittel.style.textAlign=""
+    let tittel = document.querySelector("h1");
+    tittel.innerHTML = "Velkommen til denne siden";
+    tittel.style.backgroundColor = "navy";
+    tittel.style.color = "white";
+    tittel.style.padding = "10px";
+    tittel.style.textAlign = ""
 
-document.querySelectorAll("h3+p").style.backgroundColor="silver"; 
+    document.querySelector("h3+p").style.backgroundColor = "silver";
+
+    // create a new div element
+    const newDiv = document.createElement("div");
+
+    // and give it some content
+    const newContent = document.createTextNode("Og her kommer enda mer tekst!");
+
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+
 
 }
