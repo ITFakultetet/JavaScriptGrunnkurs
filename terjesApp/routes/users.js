38c781b165a6f8cb4b742a31b00fb9c7c5dd3e8c
terjebh <terje@itfakultetet.no>
2021-10-21 13:31:55 +0200

    brukere

--- terjesApp/routes/users.js
+++ terjesApp/routes/users.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users page. */
-router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Brukere' });
+router.get('/', function (req, res, next) {
+
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = "mongodb://noderia.com/kurs?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+
+  client.connect(err => {
+    console.log("Connected successfully to server");
+    const col = client.db("kurs").collection("kursbrukere");
+
+    col.find({}).toArray(function (err, docs) {
+
+      res.render('users', {
+        title: 'Brukeroversikt',
+        docs: docs
+      });
+
+      client.close();
+    });
+
+  });
+
+
 });
 
 module.exports = router;
