0458d84a42be4bd9a912a957f2b7b53c083d56f8
terjebh <terje@itfakultetet.no>
2023-09-27 21:10:16 +0200

    Ekdempler

--- CSS-selector/selector.js
+++ CSS-selector/selector.js
@@ -1,20 +1,19 @@
-window.onload = function () {
-
     let tittel = document.querySelector("h1");
     tittel.innerHTML = "Velkommen til denne siden";
     tittel.style.backgroundColor = "navy";
+    tittel.style.borderRadius = "10px";
     tittel.style.color = "white";
     tittel.style.padding = "10px";
     tittel.style.textAlign = "";
 
-    let avsnitt1 = document.querySelector("#seksjon2>h3+p")
-
-    avsnitt1.classList.add("silver");
+    
 
     const parentElement = document.querySelector('#artikkel1>section');
     let allChildren = parentElement.querySelectorAll(":scope > p");
     allChildren.forEach(item => item.classList.add("silver"));
-  
+    
+    let avsnitt1 = document.querySelector("#seksjon2>h3+p")
+    avsnitt1.classList.add("silver");
 
     // create a new div element
     const newDiv = document.createElement("div");
@@ -28,6 +27,3 @@ window.onload = function () {
     // add the newly created element and its content into the DOM
     const currentDiv = document.getElementById("div2");
     document.body.insertBefore(newDiv, currentDiv);
-
-
-}
