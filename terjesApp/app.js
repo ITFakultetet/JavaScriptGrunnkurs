91bea980d6f7f32540d3558ba9fa16f9222ae431
terjebh <terje@itfakultetet.no>
2024-06-22 11:43:15 +0200

    eksempelapp

--- terjesApp/app.js
+++ terjesApp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newuserRouter = require('./routes/newuser');
 var enheterRouter = require('./routes/enheter');
+var ansatteRouter = require('./routes/ansatte');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/newuser', newuserRouter);
 app.use('/enheter', enheterRouter);
+app.use('/ansatte', ansatteRouter);
 
 
 // catch 404 and forward to error handler
---
+++ terjesApp/routes/ansatte.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+// Last inn PostgreSQL-driveren og opprett en tilkobling til en eksisterende PostgreSQL-server
+
+var db = require('pg');
+var dbConnection = "postgres://kurs:kurs123@s1.itfakultetet.no:5432/hr";
+
+// Her er koden som henter data og sender dem til ejs-malen
+
+router.get('/', function (req, res, next) {
+
+    var dbClient = new db.Client(dbConnection);
+    dbClient.connect(function (err) {
+
+        if (err)
+            throw err;
+
+        var query = "select concat(first_name,' ',last_name) as Navn, extract(year from birth_date) as Fodt, gender as Kjonn from employees limit 1000";
+
+        dbClient.query(query, function (err, result) {
+
+
+            if (err) {
+
+                throw err;
+
+            } else {
+
+                res.render('ansatte.ejs', {
+
+                    title: 'Ansatte',
+
+                    ansatte: result
+
+                });
+
+                res.end();
+
+            }
+
+        });
+
+    });
+
+});
+
+module.exports = router;
+
