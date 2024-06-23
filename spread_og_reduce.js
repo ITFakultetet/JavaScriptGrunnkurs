2fb4844791578bff47d1c1d47e4de519157509c2
terjebh <terje@itfakultetet.no>
2024-06-23 10:57:46 +0200

    eksempler på bruk av spread

--- spread_og_reduce.js
+++ spread_og_reduce.js
@@ -1,33 +1,22 @@
-esversion: 6;
-// Eksempler på bruk av reduce()
 
-// Eksempel 1: Beregne saldo for en konto
-/*
-let konto = [
-    { "nummer": 1001, "transaksjon": 600 },
-    { "nummer": 1002, "transaksjon": 300 },
-    { "nummer": 1002, "transaksjon": 700 },
-    { "nummer": 1001, "transaksjon": -200 },
-    { "nummer": 1001, "transaksjon": -600 },
-    { "nummer": 1001, "transaksjon": 6000 }
-];
-
-let saldo = konto.filter(x => x.nummer === 1001).map(x => x.transaksjon).reduce((x, y) => x + y);
-
-console.log("Saldo for konto 1001:", saldo);
-
-// Output:
-// Saldo for konto 1001: 5800
-*/
-// Eksempel 2: Funksjon for summering av ukjent antall parametre 
+// Eksempel 1: Funksjon for summering av ukjent antall parametre 
 // ved hjelp av spread (...) og reduce()
-const summer = (...tall) => [...tall].reduce((x, y) => x + y);
+const summer = (...tall) => [...tall].reduce((x, y) => x + y,0);
 const summen = summer(3,45,65,78);
-console.log("Summen er:", summen); // 58
+console.log("Summen er:", summen); // 191
 
 
-// Eksempel 3: Funksjon for beregning av produkt av ukjent antall parametre 
+// Eksempel 2: Funksjon for beregning av produkt av ukjent antall parametre 
 // ved hjelp av spread (...) og reduce()
-const produkt = (...tall) => [...tall].reduce((x, y) => x * y);
+const produkt = (...tall) => [...tall].reduce((x, y) => x * y,1);
 const produktet = produkt(3, 4, 6, 45);
 console.log("Produktet er:", produktet); // 3240
+
+//Eksempel 3
+// spread (...) for å slå sammen to arrays
+const biler1 = ['Volvo','Mercedes','Honda'];
+const biler2 = ['Nissan','Volkswagen','Renault'];
+const biler = [...biler1,...biler2];
+console.log(biler);   // [ 'Volvo', 'Mercedes', 'Honda', 'Nissan', 'Volkswagen', 'Renault' ]
+
+
